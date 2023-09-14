49bc30cf7c36dc19a613f9f44f65ee0a3d38266e
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-13 16:07:36 -1000

    completed 1-4

--- If Else Statements.playground/Contents.swift
+++ If Else Statements.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+var passingGrade = "I've been doing all my class work"
+
+//create our first if statement
+if passingGrade == "I've been doing all my class work"
+{
+   //if block
+    print("I should be getting an passing grade this point")
+}
+//Part 3
+else
+{
+    //else block
+    print("I need to work harder in next assignment")
+}
+
+//Part 4
+var gradeA = true
+if gradeA == true
+{
+    print("I really like coding in swift")
+}
+else
+{
+    print("I'm not so sure about coding in Swift")
+}
